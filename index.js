f50e9061c83b594594f85a2f8cfd34cdc33d12c5
anywaybetter <anywaybetterdotx@gmail.com>
2024-07-16 19:37:57 +0300

    1

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log(typeof undefined);  
+console.log(typeof null);       
+console.log(typeof true);      
+console.log(typeof 42);
+console.log(typeof 'Hello');    
+console.log(typeof {});         
+console.log(typeof []);        
+console.log(typeof function(){});
+console.log(typeof Symbol()); 
